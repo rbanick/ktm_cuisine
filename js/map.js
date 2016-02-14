160141e532d33743c2b1dcaab7faa96534605da9
Robert Banick <rbanick@gmail.com>
2016-02-14 22:04:02 +0545

    removing popupContents for the moment

--- js/map.js
+++ js/map.js
@@ -15,8 +15,6 @@
 
 // define popups as custom
 
-  var popupContents = {"<br> <b>Name: </b>" +feature.properties.name+ "<br> <b>Cuisine: </b>" +feature.properties.cuisine+ "<br> <b>Specialties: </b>" +feature.properties.specialties+ "<br> <b>Directions: </b>" +feature.properties.directions+ "<br> <b>Comments: </b>" +feature.properties.comments};
-
   var customOptions = {'className' : 'custom'};
 
 // creates geojson with basic popUp functionality
@@ -24,7 +22,7 @@
   $.getJSON('data/restaurants.json', function(data) {
       var geojson = L.geoJson(data, {
         onEachFeature: function (feature, layer) {
-          layer.bindPopup(popupContents,customOptions);
+          layer.bindPopup("<br> <b>Name: </b>" +feature.properties.name+ "<br> <b>Cuisine: </b>" +feature.properties.cuisine+ "<br> <b>Specialties: </b>" +feature.properties.specialties+ "<br> <b>Directions: </b>" +feature.properties.directions+ "<br> <b>Comments: </b>" +feature.properties.comments,customOptions);
         }
       });
 
